2bd7a63f85c9133d78a9a92fafd942060da4664f
johnmanjohnston <c.ethanjohn@gmail.com>
2024-11-27 16:53:25 +0400

    change how fonts are returned in getInterBold()

--- src/controlbar.cpp
+++ src/controlbar.cpp
@@ -3,6 +3,7 @@
 jelodyne::ControlBar::ControlBar() : juce::Component() {
     filenameLabel.setColour(juce::Label::textColourId,
                             juce::Colours::lightgrey);
+
     filenameLabel.setFont(getInterBold());
     addAndMakeVisible(filenameLabel);
 }
@@ -20,11 +21,9 @@ void jelodyne::ControlBar::resized() {
 }
 
 juce::Font jelodyne::ControlBar::getInterBold() {
-    juce::Typeface::Ptr typeface = juce::Typeface::createSystemTypefaceFor(
+    auto typeface = juce::Typeface::createSystemTypefaceFor(
         BinaryData::Inter_24ptBoldItalic_ttf,
         BinaryData::Inter_24ptBoldItalic_ttfSize);
-    ScopedPointer<Font> font = new Font(typeface);
-    font->setHeight(18.f);
 
-    return *font;
+    return Font(FontOptions(typeface).withHeight(16.f));
 }
